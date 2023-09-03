a697f9271cd206c7cac08cc256db6f4b14fe4d31
OlaMohamedd <138399376+OlaMohamedd@users.noreply.github.com>
2023-09-04 00:17:54 +0300

    starting fashion

--- Amazon/src/App.jsx
+++ Amazon/src/App.jsx
@@ -1,13 +1,32 @@
+import Fashion from "./Component/Fashion/Fashion"
+import KidsBaby from "./Component/Fashion/KidsBaby"
+import Men from "./Component/Fashion/men"
 import Mobile from "./Component/MobilePhone/mobile"
-import Navbar from "./Component/Navbar/Navbar"
+import AppLayout from "./Component/AppLayout/AppLayout"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
+import Home from "./Component/Home/Home"
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout/>,
+    children: [
+    
+      { index: true, element: <Home/>},
+      { path: "/mobile", element: <Mobile/>},
+      { path: "/fashion",
+       element: <Fashion/>,
+      children:[
+        {index:true ,element:<Fashion/>},
+        {path:'kids',element:<KidsBaby/>},
+        {path:'men',element:<Men/>},]},
+    ]
+  }
+])
 
 function App() {
-
-
   return (
     <>
-    <Navbar/>
-    <Mobile/>
+   <RouterProvider router={router} />
     </>
   )
 }
---
+++ Amazon/src/Component/Fashion/KidsBaby.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function KidsBaby() {
+  return (
+    <div>Kids&</div>
+  )
+}
