ade8b4a7401d173a2721ccb872363e746bdefdca
OlaMohamedd <138399376+OlaMohamedd@users.noreply.github.com>
2023-09-05 01:41:18 +0300

    update fashion 1

--- Amazon/src/Component/Fashion/Fashion.jsx
+++ Amazon/src/Component/Fashion/Fashion.jsx
@@ -2,33 +2,192 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
+import { BsStarFill } from "react-icons/bs";
+import { BsStar } from "react-icons/bs";
 
 export default function Fashion() {
   return (
     <>
-    
-    
-    <Navbar bg="light" data-bs-theme="light">
+
+
+      <Navbar bg="light" data-bs-theme="light">
         <Container>
-          <Navbar.Brand href="#home">Amazon Fashion</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavLink style={{ textDecoration: "none", marginLeft: '6px', color: 'black' }} to="/">Amazon Fashion</NavLink>
+            <NavLink style={{ textDecoration: "none", marginLeft: '6px', color: 'black' }} to="kids">Kids & Baby</NavLink>
+            <NavLink style={{ textDecoration: "none", marginLeft: '6px', color: 'black' }} to="men">Men</NavLink>
           </Nav>
         </Container>
       </Navbar>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+      <div className="row container-fluid m-0 justify-content-center">
+        {/* left Side  */}
+        <div className="col-2 sideLeft">
+          <div className="col">
+            <p className="h6 fw-bold">Eligible htmlFor free delivery</p>
+
+            <li className="list-group-item">
+              <input className="form-check-input me-1" type="checkbox" value="" id="f1" />Free Shipping
+              <label className="htmlForm-check-label stretched-link w-100" htmlFor="f1">
+                All customers get FREE Shipping on orders shipped by Amazon</label>
+            </li>
+          </div>
+
+          
+          <ul className="list-group side">
+            <br />
+            <p className="h6 fw-bold">Category</p>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+              </svg> Fashion
+            </span>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+              </svg> Men
+            </span>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+              </svg> Women
+            </span>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+              </svg> Girls
+            </span>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+              </svg> Baby
+            </span>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left"
+                viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                  d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z" />
+              </svg> Boys
+            </span>
+            <p className="h6 fw-bold">Fulfilled by Amazon</p>
+            <span>
+              <input className="form-check-input me-0" type="checkbox" value="" id="g1" />
+              Fulfilled by Amazon<label className="htmlForm-check-label stretched-link w-100 " htmlFor="g1"></label>
+            </span>
+          </ul>
+
+          {/* Customer Review   */}
+
+          <p className="h6 fw-bold">Color</p>
+
+<div>
+  <div className="square black"></div>
+  <div className="square gray"></div>
+  <div className="square white"></div>
+  <div className="square bro"></div>
+  <div className="square red"></div>
+  <div className="square pink"></div>
+  <div className="square orange"></div>
+  <div className="square yellow"></div>
+  <div className="square green"></div>
+  <div className="square blue"></div>
+  <div className="square darkblue"></div>
+  <div className="square purple"></div>
+  <div className="square bage"></div>
+  <div className="square bage1"></div>
+  <div className="square bage2"></div>
+
+</div>
+
+
+          <div>
+            <p className="h6 fw-bold">Avg. Customer Review</p>
+            <div className="d-inline-block">
+              <BsStarFill   color='#FFA41C' />
+              <BsStarFill   color='#FFA41C'/>
+              <BsStarFill   color='#FFA41C'/>
+              <BsStarFill   color='#FFA41C'/>
+              <BsStarFill   color='#FFA41C'/>
+
+            </div><small className="fw-bold">Only</small>
+          </div>
+          <div>
+            <div className="d-inline-block">
+              <BsStarFill  color='#FFA41C'/>
+              <BsStarFill  color='#FFA41C'/>
+              <BsStarFill  color='#FFA41C'/>
+              <BsStarFill  color='#FFA41C'/>
+              <BsStar      color='#FFA41C'/>
+            </div><small className="fw-bold">& Up</small>
+          </div>
+          <div>
+            <div>
+              <div className="d-inline-block">
+                <BsStarFill  color='#FFA41C'/>
+                <BsStarFill  color='#FFA41C'/>
+                <BsStarFill  color='#FFA41C'/>
+                <BsStar      color='#FFA41C'/>
+                <BsStar      color='#FFA41C'/>
+              </div><small className="fw-bold">& Up</small>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div className="d-inline-block">
+                <BsStarFill color='#FFA41C' />
+                <BsStarFill color='#FFA41C' />
+                <BsStar     color='#FFA41C'/>
+                <BsStar     color='#FFA41C' />
+                <BsStar     color='#FFA41C' />
+              </div><small className="fw-bold">& Up</small>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>
+                <div className="d-inline-block">
+                  <BsStarFill  color='#FFA41C'/>
+                  <BsStar  color='#FFA41C'/>
+                  <BsStar  color='#FFA41C'/>
+                  <BsStar  color='#FFA41C'/>
+                  <BsStar  color='#FFA41C'/>
+                </div><small className="fw-bold">& Up</small>
+              </div>
+            </div>
+          </div>
+          {/* End  Customer Review   */}
+        </div>
+        {/* Right side  */}
+        < div className="col-10  sideRight justify-content-center align-content-center " >
+
+
+
+
+
+        </div >
+
+
+
+      </div>
+
+
+
+
+
+
+
+
     </>
   )
 }
--- Amazon/src/Component/Navbar/Navbar.jsx
+++ Amazon/src/Component/Navbar/Navbar.jsx
@@ -1,15 +1,127 @@
+import './Navbar.css'
+import { MdLocationPin } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
+import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
     <>
-    
-    
-    
-    
-    
-    
-    
-    
+
+      <div className="container-fluid p-2 bg-dark">
+        <div className="row m-0 align-items-center">
+          <div className="col-12 col-md-2 logo d-flex flex-wrap">
+            <img className="col-6" src="../assets/images/logoo1.png" alt="logo" />
+            <div className="ps-3 col-6 d-flex align-items-center">
+              <div className=""><MdLocationPin color='white' size={20} /></div>
+              <div className="text-white d-flex flex-column ps-2">
+                <span className="text-white-50 fs-6">Deliver to</span>
+                <span className="fw-bold fs-">EGYPT</span>
+              </div>
+            </div>
+
+          </div>
+          <div className="col-12 col-md-6 py-2 py-md-0 pe-0">
+            <div className="search-container">
+              <select className="search-select ">
+                <option>All</option>
+              </select>
+              <input type="text" className="search-input " placeholder="Search Amazon for iti" />
+              <div className="search-icon">
+                <FaSearch />
+              </div>
+
+            </div>
+          </div>
+          <div className="col-12 col-md-4">
+            <ul className="list-unstyled  d-flex flex-wrap text-white align-items-center justify-content-evenly m-0">
+              <li className="col-2 justify-content-start m-0">
+                <div className="dropdown">
+                  <a className="btn text-light dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="./img/egypt_round_icon_640.png" width="20px" alt="" />
+                    EG
+                  </a>
+                  <ul className="dropdown-menu px-2 " aria-labelledby="dropdownMenuLink">
+                    <li className="py-2">
+                      <input type="radio" name="lang" id="ar" />
+                      <img src="../assets/images/flag1.png" width="19px" alt="" />
+                      <label htmlFor="ar">Arabic</label>
+                    </li>
+                    <li className="py-2">
+                      <input type="radio" name="lang" id="fr" />
+                      <img src="../assets/images/flag2.png" width="19px" alt="" />
+                      <label htmlFor="fr">France</label>
+                    </li>
+                    <li className="py-2">
+                      <input type="radio" name="lang" id="en" />
+                      <img src="../assets/images/flag3.png" width="19px" alt="" />
+                      <label htmlFor="en">English (US)</label>
+                    </li>
+
+                  </ul>
+                </div>
+              </li>
+              <li className="col-5">
+                <div className="dropdown">
+                  <a className="btn text-white dropdown-toggle text-start" href="#" role="button" id="dropdownMenuLink"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    <span className="fw-normal">Hello,sign in</span> <br />
+                    <span className="fw-bolder">Account & Lists</span>
+                  </a>
+
+                  <div className="dropdown-menu text-center" aria-labelledby="dropdownMenuLink">
+                    <button type="button" className="btn btn-warning text-center">Warning</button>
+                    <h2>dsfghjk</h2>
+                    <div className="d-flex w-600px">
+                      <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore maxime
+                          enim
+                          cum nam dolor, incidunt quis iure maiores, dolore impedit
+                          , hic sunt distinctio earum eaque officia perspiciatis tenetur
+                          perferendis
+                          porro.</p>
+                      </div>
+                      <div>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde magni nemo
+                          suscipit, quia dicta odio, ratione quos illum omnis quis hic la
+                          bore voluptatibus. Tempora expedita libero necessitatibus nesciunt quas
+                          minima.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li className="col-2">
+                <div>
+                  <span className="text-white-50 fs-6">Returns</span> <br />
+                  <span className="fw-bold fs-">&Orders</span>
+                </div>
+              </li>
+              <li className="col-3">
+                <FaShoppingCart color='white' size={25} />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      {/* sec nav bar */}
+      <div className="container-fluid  navv p-2">
+        <div className=" d-flex justify-content-between  ">
+          <div className="">
+            <NavLink className="links" to="mobile" style={{ textDecoration: "none", marginLeft: '6px' }}>Mobile Phone</NavLink>
+            <NavLink className="links" to="fashion" style={{ textDecoration: "none", marginLeft: '6px' }}>Fashion</NavLink>
+          </div>
+          <div className=" text-white">Shop deals in Electronics</div>
+        </div>
+
+      </div>
+
+
+
+
+
+
     </>
   )
 }
