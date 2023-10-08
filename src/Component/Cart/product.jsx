f7d7622640c49c66988150d8bd33709f00f7d6df
ahmed elhoseiny <ahmedelhoseiny5555@gmail.com>
2023-10-08 21:29:27 +0300

    1

--- src/Component/Cart/product.jsx
+++ src/Component/Cart/product.jsx
@@ -35,7 +35,7 @@ export default function Product(props) {
 
     return (
         <>
-            <div className="p-3  d-flex ">
+            <div className="p-3  d-flex " >
                 <img src={props.item.product.img} style={{ width: "200px" }} />
                 <div className="product-details px-2 ">
                     <p style={{ fontSize: "1.2rem", fontWeight: "500" }} className="m-1  cart-max-line" >{props.item.product.title_en}</p>
@@ -48,7 +48,7 @@ export default function Product(props) {
                     <div className="d-flex align-items-center ">
                         <p className="m-1">quantity </p>
                         <input onChange={(event) => { updateQuantity(event) }} type="number"  value={quantity} min="1" max={props.item.product.quantity} className="form-control mx-3" style={{ width: "80px", height: "30px" }} />
-                        <a className="mx-3" href="#" onClick={deleteItem}>delete</a>
+                        <a className="mx-3"  onClick={deleteItem}>delete</a>
                         <a className="mx-3" href="" onClick={() => { }}>save</a>
                     </div>
                 </div>
