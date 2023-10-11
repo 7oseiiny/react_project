ab711ea8489e549b5807d0328bef4a44f21ba045
ahmed elhoseiny <ahmedelhoseiny5555@gmail.com>
2023-10-11 21:21:01 +0300

    Update Books.jsx

--- src/Component/books/Books.jsx
+++ src/Component/books/Books.jsx
@@ -1,8 +1,7 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Books.css";
-import FormCheckInput from "react-bootstrap/esm/FormCheckInput";
-import { FaRegStar, FaStar } from "react-icons/fa6";
 import Rate from "../rate/rate";
 import { PaginationControl } from "react-bootstrap-pagination-control";
 import { useTranslation } from "react-i18next";
@@ -66,7 +65,7 @@ export default function Books() {
         }
       }
     } catch (err) {
-      console.log(err);
+      // console.log(err);
     }
   }
   function isinfav(bookId) {
@@ -84,7 +83,7 @@ export default function Books() {
   function viewcart() {
     navigate("/cart");
   }
-  const [screenWidth, setScreen] = useState(window.innerWidth);
+  const [screenWidth] = useState(window.innerWidth);
 
   var outhors = [
     "1",
